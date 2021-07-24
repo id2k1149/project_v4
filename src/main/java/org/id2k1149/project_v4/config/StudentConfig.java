d05b3cd97e82247824a6e2b77e4685f2b69db2cf
id2k1149 <id2k1149@gmail.com>
2021-07-24 17:53:00 -0400

    v0

--- src/main/java/org/id2k1149/project_v4/config/StudentConfig.java
+++ src/main/java/org/id2k1149/project_v4/config/StudentConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import static java.time.Month.*;
@@ -21,15 +20,15 @@ public class StudentConfig {
             Student john = new Student(
                     "John",
                     "john@test.test",
-                    LocalDate.of(1940, OCTOBER, 9),
-                    40
+                    LocalDate.of(1940, OCTOBER, 9)
+
             );
 
             Student paul =new Student(
                     "Paul",
                     "paul@test.test",
-                    LocalDate.of(1942, JUNE, 18),
-                    79
+                    LocalDate.of(1942, JUNE, 18)
+
             );
 
             studentRepository.saveAll(

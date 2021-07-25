e6c91833190725032538e6eea791542b536ca8f7
id2k1149 <id2k1149@gmail.com>
2021-07-24 20:15:08 -0400

    v01

--- src/main/java/org/id2k1149/project_v4/repository/StudentRepository.java
+++ src/main/java/org/id2k1149/project_v4/repository/StudentRepository.java
@@ -2,9 +2,15 @@ package org.id2k1149.project_v4.repository;
 
 import org.id2k1149.project_v4.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
